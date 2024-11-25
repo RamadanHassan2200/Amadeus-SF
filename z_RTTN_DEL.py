c983246a17244559214959fd8d7931e4d588e0f2
RamadanHassan2200 <ramadan.hassan2200@gmail.com>
2024-11-26 00:23:23 +0200

    Update z_RTTN_DEL.py

--- z_RTTN_DEL.py
+++ z_RTTN_DEL.py
@@ -3,6 +3,7 @@ capture line:2, column:5, length:2 assign to FA1
 if (FA1=="FA"){
   capture line:2, column:2, length:2 assign to XFA1
   capture line:2, column:26, length:3 assign to XVA1
+  capture line:2, column:29, length:3 assign to XVA1A
 }
 if (FA1=="FH"){
   assign "FH" to FHECheck
@@ -12,6 +13,7 @@ capture line:3, column:5, length:2 assign to FA2
 if (FA2=="FA"){
   capture line:3, column:2, length:2 assign to XFA2
   capture line:3, column:26, length:3 assign to XVA2
+  capture line:3, column:29, length:3 assign to XVA2A
 }
 if (FA2=="FH"){
   assign "FH" to FHECheck
@@ -21,6 +23,7 @@ capture line:4, column:5, length:2 assign to FA3
 if (FA3=="FA"){
   capture line:4, column:2, length:2 assign to XFA3
   capture line:4, column:26, length:3 assign to XVA3
+  capture line:4, column:29, length:3 assign to XVA3A
 }
 if (FA3=="FH"){
   assign "FH" to FHECheck
@@ -30,6 +33,7 @@ capture line:5, column:5, length:2 assign to FA4
 if (FA4=="FA"){
   capture line:5, column:2, length:2 assign to XFA4
   capture line:5, column:26, length:3 assign to XVA4
+  capture line:5, column:29, length:3 assign to XVA4A
 }
 if (FA4=="FH"){
   assign "FH" to FHECheck
@@ -39,6 +43,7 @@ capture line:6, column:5, length:2 assign to FA5
 if (FA5=="FA"){
   capture line:6, column:2, length:2 assign to XFA5
   capture line:6, column:26, length:3 assign to XVA5
+  capture line:6, column:29, length:3 assign to XVA5A
 }
 if (FA5=="FH"){
   assign "FH" to FHECheck
@@ -48,6 +53,7 @@ capture line:7, column:5, length:2 assign to FA6
 if (FA6=="FA"){
   capture line:7, column:2, length:2 assign to XFA6
   capture line:7, column:26, length:3 assign to XVA6
+  capture line:7, column:29, length:3 assign to XVA6A
 }
 if (FA6=="FH"){
   assign "FH" to FHECheck
@@ -57,6 +63,7 @@ capture line:8, column:5, length:2 assign to FA7
 if (FA7=="FA"){
   capture line:8, column:2, length:2 assign to XFA7
   capture line:8, column:26, length:3 assign to XVA7
+  capture line:8, column:29, length:3 assign to XVA7A
 }
 if (FA7=="FH"){
   assign "FH" to FHECheck
@@ -66,6 +73,7 @@ capture line:9, column:5, length:2 assign to FA8
 if (FA8=="FA"){
   capture line:9, column:2, length:2 assign to XFA8
   capture line:9, column:26, length:3 assign to XVA8
+  capture line:9, column:29, length:3 assign to XVA8A
 }
 if (FA8=="FH"){
   assign "FH" to FHECheck
@@ -75,6 +83,7 @@ capture line:10, column:5, length:2 assign to FA9
 if (FA9=="FA"){
   capture line:10, column:2, length:2 assign to XFA9
   capture line:10, column:26, length:3 assign to XVA9
+  capture line:10, column:29, length:3 assign to XVA9A
 }
 if (FA9=="FH"){
   assign "FH" to FHECheck
@@ -84,6 +93,7 @@ capture line:11, column:5, length:2 assign to FA10
 if (FA10=="FA"){
   capture line:11, column:2, length:2 assign to XFA10
   capture line:11, column:26, length:3 assign to XVA10
+  capture line:11, column:29, length:3 assign to XVA10A
 }
 if (FA10=="FH"){
   assign "FH" to FHECheck
@@ -94,6 +104,7 @@ capture line:12, column:5, length:2 assign to FA11
 if (FA11=="FA"){
   capture line:12, column:2, length:2 assign to XFA11
   capture line:12, column:26, length:3 assign to XVA11
+  capture line:12, column:29, length:3 assign to XVA11A
 }
 if (FA11=="FH"){
   assign "FH" to FHECheck
@@ -104,6 +115,7 @@ capture line:13, column:5, length:2 assign to FA12
 if (FA12=="FA"){
   capture line:13, column:2, length:2 assign to XFA12
   capture line:13, column:26, length:3 assign to XVA12
+  capture line:13, column:29, length:3 assign to XVA12A
 }
 if (FA12=="FH"){
   assign "FH" to FHECheck
@@ -114,6 +126,7 @@ capture line:14, column:5, length:2 assign to FA13
 if (FA13=="FA"){
   capture line:14, column:2, length:2 assign to XFA13
   capture line:14, column:26, length:3 assign to XVA13
+  capture line:14, column:29, length:3 assign to XVA13A
 }
 if (FA13=="FH"){
   assign "FH" to FHECheck
@@ -124,6 +137,7 @@ capture line:15, column:5, length:2 assign to FA14
 if (FA14=="FA"){
   capture line:15, column:2, length:2 assign to XFA14
   capture line:15, column:26, length:3 assign to XVA14
+  capture line:15, column:29, length:3 assign to XVA14A
 }
 if (FA14=="FH"){
   assign "FH" to FHECheck
@@ -134,6 +148,7 @@ capture line:16, column:5, length:2 assign to FA15
 if (FA15=="FA"){
   capture line:16, column:2, length:2 assign to XFA15
   capture line:16, column:26, length:3 assign to XVA15
+  capture line:16, column:29, length:3 assign to XVA15A
 }
 if (FA15=="FH"){
   assign "FH" to FHECheck
@@ -144,6 +159,7 @@ capture line:17, column:5, length:2 assign to FA16
 if (FA16=="FA"){
   capture line:17, column:2, length:2 assign to XFA16
   capture line:17, column:26, length:3 assign to XVA16
+  capture line:17, column:29, length:3 assign to XVA16A
 }
 if (FA16=="FH"){
   assign "FH" to FHECheck
@@ -154,6 +170,7 @@ capture line:18, column:5, length:2 assign to FA17
 if (FA17=="FA"){
   capture line:18, column:2, length:2 assign to XFA17
   capture line:18, column:26, length:3 assign to XVA17
+  capture line:18, column:29, length:3 assign to XVA17A
 }
 if (FA17=="FH"){
   assign "FH" to FHECheck
@@ -164,6 +181,7 @@ capture line:19, column:5, length:2 assign to FA18
 if (FA18=="FA"){
   capture line:19, column:2, length:2 assign to XFA18
   capture line:19, column:26, length:3 assign to XVA18
+  capture line:19, column:29, length:3 assign to XVA18A
 }
 if (FA18=="FH"){
   assign "FH" to FHECheck
@@ -174,6 +192,7 @@ capture line:20, column:5, length:2 assign to FA19
 if (FA19=="FA"){
   capture line:20, column:2, length:2 assign to XFA19
   capture line:20, column:26, length:3 assign to XVA19
+  capture line:20, column:29, length:3 assign to XVA19A
 }
 if (FA19=="FH"){
   assign "FH" to FHECheck
@@ -184,6 +203,7 @@ capture line:21, column:5, length:2 assign to FA20
 if (FA20=="FA"){
   capture line:21, column:2, length:2 assign to XFA20
   capture line:21, column:26, length:3 assign to XVA20
+  capture line:21, column:29, length:3 assign to XVA20A
 }
 if (FA20=="FH"){
   assign "FH" to FHECheck
@@ -194,6 +214,7 @@ capture line:22, column:5, length:2 assign to FA21
 if (FA21=="FA"){
   capture line:22, column:2, length:2 assign to XFA21
   capture line:22, column:26, length:3 assign to XVA21
+  capture line:22, column:29, length:3 assign to XVA21A
 }
 if (FA21=="FH"){
   assign "FH" to FHECheck
@@ -204,6 +225,7 @@ capture line:23, column:5, length:2 assign to FA22
 if (FA22=="FA"){
   capture line:23, column:2, length:2 assign to XFA22
   capture line:23, column:26, length:3 assign to XVA22
+  capture line:23, column:29, length:3 assign to XVA22A
 }
 if (FA22=="FH"){
   assign "FH" to FHECheck
@@ -213,6 +235,7 @@ capture line:24, column:5, length:2 assign to FA23
 if (FA23=="FA"){
   capture line:24, column:2, length:2 assign to XFA23
   capture line:24, column:26, length:3 assign to XVA23
+  capture line:24, column:29, length:3 assign to XVA23A
 }
 if (FA23=="FH"){
   assign "FH" to FHECheck
@@ -227,11 +250,24 @@ if (FHECheck=="FH"){
         send "XE" +XFHE
           call "z_RTTN_DEL"
       }
+      if (FST=="R"){
+        send "XE" +XFHE
+          call "z_RTTN_DEL"
+      }
 }
 
 if (FA1=="FA"){
 if (FA23=="FA"){
   if (XVA23=="/EV"){
+  send "XE" +XFA1 +"-" +XFA23
+  }
+  if (XVA23=="/ER"){
+  send "XE" +XFA1 +"-" +XFA23
+  }
+  if (XVA23A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA23
+  }
+  if (XVA23A=="/ER"){
   send "XE" +XFA1 +"-" +XFA23
   }
 }
@@ -240,10 +276,28 @@ if (FA22=="FA"){
   if (XVA22=="/EV"){
   send "XE" +XFA1 +"-" +XFA22
   }
+  if (XVA22=="/ER"){
+  send "XE" +XFA1 +"-" +XFA22
+  }
+  if (XVA22A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA22
+  }
+  if (XVA22A=="/ER"){
+  send "XE" +XFA1 +"-" +XFA22
+  }
 }
 else{
 if (FA21=="FA"){
   if (XVA21=="/EV"){
+  send "XE" +XFA1 +"-" +XFA21
+  }
+  if (XVA21=="/ER"){
+  send "XE" +XFA1 +"-" +XFA21
+  }
+  if (XVA21A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA21
+  }
+  if (XVA21A=="/ER"){
   send "XE" +XFA1 +"-" +XFA21
   }
 }
@@ -252,10 +306,28 @@ if (FA20=="FA"){
   if (XVA20=="/EV"){
   send "XE" +XFA1 +"-" +XFA20
   }
+  if (XVA20=="/ER"){
+  send "XE" +XFA1 +"-" +XFA20
+  }
+  if (XVA20A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA20
+  }
+  if (XVA20A=="/ER"){
+  send "XE" +XFA1 +"-" +XFA20
+  }
 }
 else{
 if (FA19=="FA"){
   if (XVA19=="/EV"){
+  send "XE" +XFA1 +"-" +XFA19
+  }
+  if (XVA19=="/ER"){
+  send "XE" +XFA1 +"-" +XFA19
+  }
+  if (XVA19A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA19
+  }
+  if (XVA19A=="/ER"){
   send "XE" +XFA1 +"-" +XFA19
   }
 }
@@ -264,10 +336,28 @@ if (FA18=="FA"){
   if (XVA18=="/EV"){
   send "XE" +XFA1 +"-" +XFA18
   }
+  if (XVA18=="/ER"){
+  send "XE" +XFA1 +"-" +XFA18
+  }
+  if (XVA18A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA18
+  }
+  if (XVA18A=="/ER"){
+  send "XE" +XFA1 +"-" +XFA18
+  }
 }
 else{
 if (FA17=="FA"){
   if (XVA17=="/EV"){
+  send "XE" +XFA1 +"-" +XFA17
+  }
+  if (XVA17=="/ER"){
+  send "XE" +XFA1 +"-" +XFA17
+  }
+  if (XVA17A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA17
+  }
+  if (XVA17A=="/ER"){
   send "XE" +XFA1 +"-" +XFA17
   }
 }
@@ -276,10 +366,28 @@ if (FA16=="FA"){
   if (XVA16=="/EV"){
   send "XE" +XFA1 +"-" +XFA16
   }
+  if (XVA16=="/ER"){
+  send "XE" +XFA1 +"-" +XFA16
+  }
+  if (XVA16A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA16
+  }
+  if (XVA16A=="/ER"){
+  send "XE" +XFA1 +"-" +XFA16
+  }
 }
 else{
 if (FA15=="FA"){
   if (XVA15=="/EV"){
+  send "XE" +XFA1 +"-" +XFA15
+  }
+  if (XVA15=="/ER"){
+  send "XE" +XFA1 +"-" +XFA15
+  }
+  if (XVA15A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA15
+  }
+  if (XVA15A=="/ER"){
   send "XE" +XFA1 +"-" +XFA15
   }
 }
@@ -288,10 +396,28 @@ if (FA14=="FA"){
   if (XVA14=="/EV"){
   send "XE" +XFA1 +"-" +XFA14
   }
+  if (XVA14=="/ER"){
+  send "XE" +XFA1 +"-" +XFA14
+  }
+  if (XVA14A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA14
+  }
+  if (XVA14A=="/ER"){
+  send "XE" +XFA1 +"-" +XFA14
+  }
 }
 else{
 if (FA13=="FA"){
   if (XVA13=="/EV"){
+  send "XE" +XFA1 +"-" +XFA13
+  }
+  if (XVA13=="/ER"){
+  send "XE" +XFA1 +"-" +XFA13
+  }
+  if (XVA13A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA13
+  }
+  if (XVA13A=="/ER"){
   send "XE" +XFA1 +"-" +XFA13
   }
 }
@@ -300,10 +426,28 @@ if (FA12=="FA"){
   if (XVA12=="/EV"){
   send "XE" +XFA2 +"-" +XFA12
   }
+  if (XVA12=="/ER"){
+  send "XE" +XFA2 +"-" +XFA12
+  }
+  if (XVA12A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA12
+  }
+  if (XVA12A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA12
+  }
 }
 else{
 if (FA11=="FA"){
   if (XVA11=="/EV"){
+  send "XE" +XFA1 +"-" +XFA11
+  }
+  if (XVA11=="/ER"){
+  send "XE" +XFA1 +"-" +XFA11
+  }
+  if (XVA11A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA11
+  }
+  if (XVA11A=="/ER"){
   send "XE" +XFA1 +"-" +XFA11
   }
 }
@@ -312,10 +456,28 @@ if (FA10=="FA"){
   if (XVA10=="/EV"){
   send "XE" +XFA1 +"-" +XFA10
   }
+  if (XVA10=="/ER"){
+  send "XE" +XFA1 +"-" +XFA10
+  }
+  if (XVA10A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA10
+  }
+  if (XVA10A=="/ER"){
+  send "XE" +XFA1 +"-" +XFA10
+  }
 }
 else{
 if (FA9=="FA"){
   if (XVA9=="/EV"){
+  send "XE" +XFA1 +"-" +XFA9
+  }
+  if (XVA9=="/ER"){
+  send "XE" +XFA1 +"-" +XFA9
+  }
+  if (XVA9A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA9
+  }
+  if (XVA9A=="/ER"){
   send "XE" +XFA1 +"-" +XFA9
   }
 }
@@ -324,10 +486,28 @@ if (FA8=="FA"){
   if (XVA8=="/EV"){
   send "XE" +XFA1 +"-" +XFA8
   }
+  if (XVA8=="/ER"){
+  send "XE" +XFA1 +"-" +XFA8
+  }
+  if (XVA8A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA8
+  }
+  if (XVA8A=="/ER"){
+  send "XE" +XFA1 +"-" +XFA8
+  }
 }
 else{
 if (FA7=="FA"){
   if (XVA7=="/EV"){
+  send "XE" +XFA1 +"-" +XFA7
+  }
+  if (XVA7=="/ER"){
+  send "XE" +XFA1 +"-" +XFA7
+  }
+  if (XVA7A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA7
+  }
+  if (XVA7A=="/ER"){
   send "XE" +XFA1 +"-" +XFA7
   }
 }
@@ -336,10 +516,28 @@ if (FA6=="FA"){
   if (XVA6=="/EV"){
   send "XE" +XFA1 +"-" +XFA6
   }
+  if (XVA6=="/ER"){
+  send "XE" +XFA1 +"-" +XFA6
+  }
+  if (XVA6A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA6
+  }
+  if (XVA6A=="/ER"){
+  send "XE" +XFA1 +"-" +XFA6
+  }
 }
 else{
 if (FA5=="FA"){
   if (XVA5=="/EV"){
+  send "XE" +XFA1 +"-" +XFA5
+  }
+  if (XVA5=="/ER"){
+  send "XE" +XFA1 +"-" +XFA5
+  }
+  if (XVA5A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA5
+  }
+  if (XVA5A=="/ER"){
   send "XE" +XFA1 +"-" +XFA5
   }
 }
@@ -348,10 +546,28 @@ if (FA4=="FA"){
   if (XVA4=="/EV"){
   send "XE" +XFA1 +"-" +XFA4
   }
+  if (XVA4=="/ER"){
+  send "XE" +XFA1 +"-" +XFA4
+  }
+  if (XVA4A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA4
+  }
+  if (XVA4A=="/ER"){
+  send "XE" +XFA1 +"-" +XFA4
+  }
 }
 else{
 if (FA3=="FA"){
   if (XVA3=="/EV"){
+  send "XE" +XFA1 +"-" +XFA3
+  }
+  if (XVA3=="/ER"){
+  send "XE" +XFA1 +"-" +XFA3
+  }
+  if (XVA3A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA3
+  }
+  if (XVA3A=="/ER"){
   send "XE" +XFA1 +"-" +XFA3
   }
 }
@@ -360,10 +576,28 @@ if (FA2=="FA"){
   if (XVA2=="/EV"){
   send "XE" +XFA1 +"-" +XFA2
   }
+  if (XVA2=="/ER"){
+  send "XE" +XFA1 +"-" +XFA2
+  }
+  if (XVA2A=="/EV"){
+  send "XE" +XFA1 +"-" +XFA2
+  }
+  if (XVA2A=="/ER"){
+  send "XE" +XFA1 +"-" +XFA2
+  }
 }
 else{
 if (FA1=="FA"){
   if (XVA1=="/EV"){
+  send "XE" +XFA1
+  }
+  if (XVA1=="/ER"){
+  send "XE" +XFA1
+  }
+  if (XVA1A=="/EV"){
+  send "XE" +XFA1
+  }
+  if (XVA1A=="/ER"){
   send "XE" +XFA1
   }
 }
@@ -395,10 +629,28 @@ if (FA23=="FA"){
   if (XVA23=="/EV"){
   send "XE" +XFA2 +"-" +XFA23
   }
+  if (XVA23=="/ER"){
+  send "XE" +XFA2 +"-" +XFA23
+  }
+  if (XVA23A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA23
+  }
+  if (XVA23A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA23
+  }
 }
 else{
 if (FA22=="FA"){
   if (XVA22=="/EV"){
+  send "XE" +XFA2 +"-" +XFA22
+  }
+  if (XVA22=="/ER"){
+  send "XE" +XFA2 +"-" +XFA22
+  }
+  if (XVA22A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA22
+  }
+  if (XVA22A=="/ER"){
   send "XE" +XFA2 +"-" +XFA22
   }
 }
@@ -407,10 +659,28 @@ if (FA21=="FA"){
   if (XVA21=="/EV"){
   send "XE" +XFA2 +"-" +XFA21
   }
+  if (XVA21=="/ER"){
+  send "XE" +XFA2 +"-" +XFA21
+  }
+  if (XVA21A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA21
+  }
+  if (XVA21A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA21
+  }
 }
 else{
 if (FA20=="FA"){
   if (XVA20=="/EV"){
+  send "XE" +XFA2 +"-" +XFA20
+  }
+  if (XVA20=="/ER"){
+  send "XE" +XFA2 +"-" +XFA20
+  }
+  if (XVA20A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA20
+  }
+  if (XVA20A=="/ER"){
   send "XE" +XFA2 +"-" +XFA20
   }
 }
@@ -419,10 +689,28 @@ if (FA19=="FA"){
   if (XVA19=="/EV"){
   send "XE" +XFA2 +"-" +XFA19
   }
+  if (XVA19=="/ER"){
+  send "XE" +XFA2 +"-" +XFA19
+  }
+  if (XVA19A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA19
+  }
+  if (XVA19A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA19
+  }
 }
 else{
 if (FA18=="FA"){
   if (XVA18=="/EV"){
+  send "XE" +XFA2 +"-" +XFA18
+  }
+  if (XVA18=="/ER"){
+  send "XE" +XFA2 +"-" +XFA18
+  }
+  if (XVA18A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA18
+  }
+  if (XVA18A=="/ER"){
   send "XE" +XFA2 +"-" +XFA18
   }
 }
@@ -431,10 +719,28 @@ if (FA17=="FA"){
   if (XVA17=="/EV"){
   send "XE" +XFA2 +"-" +XFA17
   }
+  if (XVA17=="/ER"){
+  send "XE" +XFA2 +"-" +XFA17
+  }
+  if (XVA17A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA17
+  }
+  if (XVA17A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA17
+  }
 }
 else{
 if (FA16=="FA"){
   if (XVA16=="/EV"){
+  send "XE" +XFA2 +"-" +XFA16
+  }
+  if (XVA16=="/ER"){
+  send "XE" +XFA2 +"-" +XFA16
+  }
+  if (XVA16A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA16
+  }
+  if (XVA16A=="/ER"){
   send "XE" +XFA2 +"-" +XFA16
   }
 }
@@ -443,10 +749,28 @@ if (FA15=="FA"){
   if (XVA15=="/EV"){
   send "XE" +XFA2 +"-" +XFA15
   }
+  if (XVA15=="/ER"){
+  send "XE" +XFA2 +"-" +XFA15
+  }
+  if (XVA15A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA15
+  }
+  if (XVA15A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA15
+  }
 }
 else{
 if (FA14=="FA"){
   if (XVA14=="/EV"){
+  send "XE" +XFA2 +"-" +XFA14
+  }
+  if (XVA14=="/ER"){
+  send "XE" +XFA2 +"-" +XFA14
+  }
+  if (XVA14A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA14
+  }
+  if (XVA14A=="/ER"){
   send "XE" +XFA2 +"-" +XFA14
   }
 }
@@ -455,10 +779,28 @@ if (FA13=="FA"){
   if (XVA13=="/EV"){
   send "XE" +XFA2 +"-" +XFA13
   }
+  if (XVA13=="/ER"){
+  send "XE" +XFA2 +"-" +XFA13
+  }
+  if (XVA13A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA13
+  }
+  if (XVA13A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA13
+  }
 }
 else{
 if (FA12=="FA"){
   if (XVA12=="/EV"){
+  send "XE" +XFA2 +"-" +XFA12
+  }
+  if (XVA12=="/ER"){
+  send "XE" +XFA2 +"-" +XFA12
+  }
+  if (XVA12A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA12
+  }
+  if (XVA12A=="/ER"){
   send "XE" +XFA2 +"-" +XFA12
   }
 }
@@ -467,10 +809,28 @@ if (FA11=="FA"){
   if (XVA11=="/EV"){
   send "XE" +XFA2 +"-" +XFA11
   }
+  if (XVA11=="/ER"){
+  send "XE" +XFA2 +"-" +XFA11
+  }
+  if (XVA11A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA11
+  }
+  if (XVA11A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA11
+  }
 }
 else{
 if (FA10=="FA"){
   if (XVA10=="/EV"){
+  send "XE" +XFA2 +"-" +XFA10
+  }
+  if (XVA10=="/ER"){
+  send "XE" +XFA2 +"-" +XFA10
+  }
+  if (XVA10A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA10
+  }
+  if (XVA10A=="/ER"){
   send "XE" +XFA2 +"-" +XFA10
   }
 }
@@ -479,10 +839,28 @@ if (FA9=="FA"){
   if (XVA9=="/EV"){
   send "XE" +XFA2 +"-" +XFA9
   }
+  if (XVA9=="/ER"){
+  send "XE" +XFA2 +"-" +XFA9
+  }
+  if (XVA9A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA9
+  }
+  if (XVA9A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA9
+  }
 }
 else{
 if (FA8=="FA"){
   if (XVA8=="/EV"){
+  send "XE" +XFA2 +"-" +XFA8
+  }
+  if (XVA8=="/ER"){
+  send "XE" +XFA2 +"-" +XFA8
+  }
+  if (XVA8A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA8
+  }
+  if (XVA8A=="/ER"){
   send "XE" +XFA2 +"-" +XFA8
   }
 }
@@ -491,10 +869,28 @@ if (FA7=="FA"){
   if (XVA7=="/EV"){
   send "XE" +XFA2 +"-" +XFA7
   }
+  if (XVA7=="/ER"){
+  send "XE" +XFA2 +"-" +XFA7
+  }
+  if (XVA7A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA7
+  }
+  if (XVA7A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA7
+  }
 }
 else{
 if (FA6=="FA"){
   if (XVA6=="/EV"){
+  send "XE" +XFA2 +"-" +XFA6
+  }
+  if (XVA6=="/ER"){
+  send "XE" +XFA2 +"-" +XFA6
+  }
+  if (XVA6A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA6
+  }
+  if (XVA6A=="/ER"){
   send "XE" +XFA2 +"-" +XFA6
   }
 }
@@ -503,10 +899,28 @@ if (FA5=="FA"){
   if (XVA5=="/EV"){
   send "XE" +XFA2 +"-" +XFA5
   }
+  if (XVA5=="/ER"){
+  send "XE" +XFA2 +"-" +XFA5
+  }
+  if (XVA5A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA5
+  }
+  if (XVA5A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA5
+  }
 }
 else{
 if (FA4=="FA"){
   if (XVA4=="/EV"){
+  send "XE" +XFA2 +"-" +XFA4
+  }
+  if (XVA4=="/ER"){
+  send "XE" +XFA2 +"-" +XFA4
+  }
+  if (XVA4A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA4
+  }
+  if (XVA4A=="/ER"){
   send "XE" +XFA2 +"-" +XFA4
   }
 }
@@ -515,10 +929,28 @@ if (FA3=="FA"){
   if (XVA3=="/EV"){
   send "XE" +XFA2 +"-" +XFA3
   }
+  if (XVA3=="/ER"){
+  send "XE" +XFA2 +"-" +XFA3
+  }
+  if (XVA3A=="/EV"){
+  send "XE" +XFA2 +"-" +XFA3
+  }
+  if (XVA3A=="/ER"){
+  send "XE" +XFA2 +"-" +XFA3
+  }
 }
 else{
 if (FA2=="FA"){
   if (XVA2=="/EV"){
+  send "XE" +XFA2
+  }
+  if (XVA2=="/ER"){
+  send "XE" +XFA2
+  }
+  if (XVA2A=="/EV"){
+  send "XE" +XFA2
+  }
+  if (XVA2A=="/ER"){
   send "XE" +XFA2
   }
 }
