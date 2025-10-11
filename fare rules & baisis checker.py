4f94f4e987d8cdb9949856a73d79e5da96036018
RamadanHassan2200 <ramadan.hassan2200@gmail.com>
2025-10-11 15:14:00 +0300

    Update fare rules & baisis checker.py

--- fare rules & baisis checker.py
+++ fare rules & baisis checker.py
@@ -124,7 +124,7 @@ capture line:4, column:33, length:1 assign to class1
 capture line:4, column:20, length:5 assign to travelDate1
 capture line:4, column:30, length:2 assign to OK1
 capture line:4, column:33, length:4 assign to fareBasis1_Shortcut
-capture line:4, column:33, length:14 assign to fareBasis1
+capture line:4, column:33, length:12 assign to fareBasis1
 capture line:4, column:47, length:1 assign to status1
 capture line:4, column:62, length:2 assign to pc1
 
@@ -135,7 +135,7 @@ capture line:5, column:11, length:4 assign to flightNo2
 capture line:5, column:33, length:1 assign to class2
 capture line:5, column:20, length:5 assign to travelDate2
 capture line:5, column:30, length:2 assign to OK2
-capture line:5, column:33, length:14 assign to fareBasis2
+capture line:5, column:33, length:12 assign to fareBasis2
 capture line:5, column:47, length:1 assign to status2
 capture line:5, column:62, length:2 assign to pc2
 
@@ -146,7 +146,7 @@ capture line:6, column:11, length:4 assign to flightNo3
 capture line:6, column:33, length:1 assign to class3
 capture line:6, column:20, length:5 assign to travelDate3
 capture line:6, column:30, length:2 assign to OK3
-capture line:6, column:33, length:14 assign to fareBasis3
+capture line:6, column:33, length:12 assign to fareBasis3
 capture line:6, column:47, length:1 assign to status3
 capture line:6, column:62, length:2 assign to pc3
 
@@ -157,7 +157,7 @@ capture line:7, column:11, length:4 assign to flightNo4
 capture line:7, column:33, length:1 assign to class4
 capture line:7, column:20, length:5 assign to travelDate4
 capture line:7, column:30, length:2 assign to OK4
-capture line:7, column:33, length:14 assign to fareBasis4
+capture line:7, column:33, length:12 assign to fareBasis4
 capture line:7, column:47, length:1 assign to status4
 capture line:7, column:62, length:2 assign to pc4
 
@@ -168,7 +168,7 @@ capture line:8, column:11, length:4 assign to flightNo5
 capture line:8, column:33, length:1 assign to class5
 capture line:8, column:20, length:5 assign to travelDate5
 capture line:8, column:30, length:2 assign to OK5
-capture line:8, column:33, length:14 assign to fareBasis5
+capture line:8, column:33, length:12 assign to fareBasis5
 capture line:8, column:47, length:1 assign to status5
 capture line:8, column:62, length:2 assign to pc5
 
@@ -179,7 +179,7 @@ capture line:9, column:11, length:4 assign to flightNo6
 capture line:9, column:33, length:1 assign to class6
 capture line:9, column:20, length:5 assign to travelDate6
 capture line:9, column:30, length:2 assign to OK6
-capture line:9, column:33, length:14 assign to fareBasis6
+capture line:9, column:33, length:12 assign to fareBasis6
 capture line:9, column:47, length:1 assign to status6
 capture line:9, column:62, length:2 assign to pc6
 
@@ -1441,7 +1441,7 @@ if (segCount=="1"){
     assign fareBasis1 to fareBasis
 }
 
-assign "P" to airline_TourCode
+assign "" to airline_TourCode
 if (airline1 == "PR"){
   assign "U*C5YQ" to airline_TourCode
 }
@@ -1460,15 +1460,12 @@ if (airline1 == "SN"){
 if (airline1 == "WY"){
   assign "U584562" to airline_TourCode
 }
-  assign "" to ptc_ID
-  if (PTC == "CHD"){
-    assign "CH" to ptc_ID
-  }
-  if (PTC == "INF"){
-    assign "IN" to ptc_ID
-  }
+if (airline1 == "RJ"){
+  assign "P" to airline_TourCode
+}
 
-  send "FQD" +city1 +FQDCommand +"/R" +ptc_ID +"," +DOI +",U*SRG01,UP," +airline_TourCode
+
+  send "FQD" +city1 +FQDCommand +"/R,up" +DOI +"," +airline_TourCode
 
 
   capture line:4, column:1, length:2 assign to FQDN1
@@ -1489,23 +1486,23 @@ if (airline1 == "WY"){
   capture line:19, column:1, length:2 assign to FQDN16
   capture line:20, column:1, length:2 assign to FQDN17
 
-  capture line:4, column:4, length:14 assign to FQDFareRule1
-  capture line:5, column:4, length:14 assign to FQDFareRule2
-  capture line:6, column:4, length:14 assign to FQDFareRule3
-  capture line:7, column:4, length:14 assign to FQDFareRule4
-  capture line:8, column:4, length:14 assign to FQDFareRule5
-  capture line:9, column:4, length:14 assign to FQDFareRule6
-  capture line:10, column:4, length:14 assign to FQDFareRule7
-  capture line:11, column:4, length:14 assign to FQDFareRule8
-  capture line:12, column:4, length:14 assign to FQDFareRule9
-  capture line:13, column:4, length:14 assign to FQDFareRule10
-  capture line:14, column:4, length:14 assign to FQDFareRule11
-  capture line:15, column:4, length:14 assign to FQDFareRule12
-  capture line:16, column:4, length:14 assign to FQDFareRule13
-  capture line:17, column:4, length:14 assign to FQDFareRule14
-  capture line:18, column:4, length:14 assign to FQDFareRule15
-  capture line:19, column:4, length:14 assign to FQDFareRule16
-  capture line:20, column:4, length:14 assign to FQDFareRule17
+  capture line:4, column:4, length:12 assign to FQDFareRule1
+  capture line:5, column:4, length:12 assign to FQDFareRule2
+  capture line:6, column:4, length:12 assign to FQDFareRule3
+  capture line:7, column:4, length:12 assign to FQDFareRule4
+  capture line:8, column:4, length:12 assign to FQDFareRule5
+  capture line:9, column:4, length:12 assign to FQDFareRule6
+  capture line:10, column:4, length:12 assign to FQDFareRule7
+  capture line:11, column:4, length:12 assign to FQDFareRule8
+  capture line:12, column:4, length:12 assign to FQDFareRule9
+  capture line:13, column:4, length:12 assign to FQDFareRule10
+  capture line:14, column:4, length:12 assign to FQDFareRule11
+  capture line:15, column:4, length:12 assign to FQDFareRule12
+  capture line:16, column:4, length:12 assign to FQDFareRule13
+  capture line:17, column:4, length:12 assign to FQDFareRule14
+  capture line:18, column:4, length:12 assign to FQDFareRule15
+  capture line:19, column:4, length:12 assign to FQDFareRule16
+  capture line:20, column:4, length:12 assign to FQDFareRule17
   
   if (FQDFareRule1==fareBasis){
     send "FQN" +FQDN1 +"*PE"
@@ -1559,33 +1556,57 @@ if (airline1 == "WY"){
     send "FQN" +FQDN17 +"*PE"
   }
 
-  send "MD-Cancellations"
+  send "FQN" +FXXfareBasisNumber +"*PE"
+ send "MD-Cancellations"
 
+  assign "1" to Fares
   if (segCount > "1"){
-    if (fareBasis2 != fareBasis1){
-      send "Please check Fare Basis for Segment 2!"
+    if (fareBasis1 != fareBasis2){
+      append "1" to Fares
     }
   }
   if (segCount > "2"){
-    if (fareBasis3 != fareBasis2){
-      send "Please check Fare Basis for Segment 3!"
+    if (fareBasis2 != fareBasis3){
+      append "1" to Fares
     }
   }
   if (segCount > "3"){
-    if (fareBasis4 != fareBasis3){
-      send "Please check Fare Basis for Segment 4!"
+    if (fareBasis3 != fareBasis4){
+      append "1" to Fares
     }
   }
   if (segCount > "4"){
-    if (fareBasis5 != fareBasis4){
-      send "Please check Fare Basis for Segment 5!"
+    if (fareBasis4 != fareBasis5){
+      append "1" to Fares
     }
   }
   if (segCount > "5"){
-    if (fareBasis6 != fareBasis5){
-      send "Please check Fare Basis for Segment 6!"
+    if (fareBasis5 != fareBasis6){
+      append "1" to Fares
     }
   }
+
+  if (Fares == "11"){
+    send "FQN" +FXXfareBasisNumber +"-2*PE"
+    send "MD-Cancellations"
+  }
+  if (Fares == "111"){
+    send "FQN" +FXXfareBasisNumber +"-3*PE"
+    send "MD-Cancellations"
+  }
+  if (Fares == "1111"){
+    send "FQN" +FXXfareBasisNumber +"-4*PE"
+    send "MD-Cancellations"
+  }
+  if (Fares == "11111"){
+    send "FQN" +FXXfareBasisNumber +"-5*PE"
+    send "MD-Cancellations"
+  }
+  if (Fares == "111111"){
+    send "FQN" +FXXfareBasisNumber +"-6*PE"
+    send "MD-Cancellations"
+  }
+
 
   }
 
@@ -1621,7 +1642,7 @@ if (airline1 == "WY"){
     }
     }
 
-    send "FQP" +FQPCommand +"/R," +DOI +",U*SRG01,UP,P/L-"  
+    send "FQP" +FQPCommand +"/R," +DOI +",UP,P/L-"  
     
         
   }
